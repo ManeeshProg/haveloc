e13d5d867b544d976e34e609ceb659efd2f4eae4
NotManeesh23 <maneeshprog@gmail.com>
2025-10-06 16:18:06 +0530

    Replace old files with new version

---
+++ Clicknium/app.py
@@ -0,0 +1,3 @@
+from clicknium import clicknium as cc, locator, ui
+
+tab=cc.chrome.open("https://app.haveloc.com/login")
---
+++ first.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver=webdriver.Chrome()
+driver.get('https://selenium.dev/documentation')
+assert 'Selenium' in driver.title
+elem=driver.find_element(By.ID,'m-documentationwebdriver')
+elem.click()
+# time.sleep(5)
+assert 'WebDriver' in driver.title
+# driver.quit()
